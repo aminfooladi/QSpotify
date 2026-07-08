c54eacaba20ab1b061ad2563e100c4289e7126da
aminfooladi <aminfooladi0891@gmail.com>
2026-07-08 13:25:33 +0330

    Account class created and wrote

---
+++ account.cpp
@@ -0,0 +1,29 @@
+#include "account.h"
+
+Account::Account() : id(0), role(Role::Listener) {}
+
+Account::Account(int id, QString fullName, QString userName, QString biography, QString password, Role role, QString profilePhoto)
+    : id(id), fullName(fullName), userName(userName),
+    biography(biography), password(password), role(role),
+    profilePhoto(profilePhoto) {}
+
+int Account::getId() const { return id; }
+void Account::setId(int id) { this->id = id; }
+
+QString Account::getFullName() const { return fullName; }
+void Account::setFullName(const QString& fullName) { this->fullName = fullName; }
+
+QString Account::getUserName() const { return userName; }
+void Account::setUserName(const QString& userName) { this->userName = userName; }
+
+QString Account::getBiography() const { return biography; }
+void Account::setBiography(const QString& biography) { this->biography = biography; }
+
+QString Account::getPassword() const { return password; }
+void Account::setPassword(const QString& password) { this->password = password; }
+
+Role Account::getRole() const { return role; }
+void Account::setRole(Role role) { this->role = role; }
+
+QString Account::getProfilePhoto() const { return profilePhoto; }
+void Account::setProfilePhoto(const QString& profilePhoto) { this->profilePhoto = profilePhoto; }
---
+++ account.h
@@ -0,0 +1,51 @@
+#ifndef ACCOUNT_H
+#define ACCOUNT_H
+
+#include <QString>
+
+enum class Role
+{
+    Artist,
+    Listener
+};
+
+class Account
+{
+private:
+    int id;
+    QString fullName;
+    QString userName;
+    QString biography;
+    QString password;
+    Role role;
+    QString profilePhoto;
+
+public:
+    Account();
+    Account(int id, QString fullName, QString userName,
+            QString biography, QString password, Role role,
+            QString profilePhoto = "");
+
+    int getId() const;
+    void setId(int id);
+
+    QString getFullName() const;
+    void setFullName(const QString& fullName);
+
+    QString getUserName() const;
+    void setUserName(const QString& userName);
+
+    QString getBiography() const;
+    void setBiography(const QString& biography);
+
+    QString getPassword() const;
+    void setPassword(const QString& password);
+
+    Role getRole() const;
+    void setRole(Role role);
+
+    QString getProfilePhoto() const;
+    void setProfilePhoto(const QString& profilePhoto);
+};
+
+#endif // ACCOUNT_H
